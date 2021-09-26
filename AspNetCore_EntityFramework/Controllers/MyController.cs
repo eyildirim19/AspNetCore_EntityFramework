8f6dfedc45d697f03e7df2275f3ea80d1f84ac8a
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-09-26 10:56:52 +0300

    Add project files.

---
+++ AspNetCore_EntityFramework/Controllers/MyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore_EntityFramework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_EntityFramework.Controllers
+{
+    public class MyController : Controller
+    {
+        CoreDBContext dBContext;
+        public MyController(CoreDBContext _dbContext)
+        {
+            dBContext = _dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Category model)
+        {
+            dBContext.Category.Add(model);
+            dBContext.SaveChanges(); // değişikliği veritana yansıt...
+
+            return View();
+        }
+    }
+}
---
+++ AspNetCore_EntityFramework/Models/CoreDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_EntityFramework.Models
+{
+    public class CoreDBContext : DbContext
+    {
+        // base sınıfa connectionstring buradan gönderilmez..DbContextOptions tipinde generic bir paramertre tanımlanır. Base sınıfa bu parametre gönderilir. ctor'daki parametre ise Startup dosyasındanki ConfigurationServies metodundan gönderilir ...
+        public CoreDBContext(DbContextOptions<CoreDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Products> Product { get; set; }
+    }
+}
